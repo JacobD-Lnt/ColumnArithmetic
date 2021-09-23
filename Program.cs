47ca0418987c1201a677689c909f2f2b8f50084b
LTIUser <LTIUser@INFEDL31192.lntinfotech.com>
2021-09-23 10:31:43 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ColumnArithmetic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
