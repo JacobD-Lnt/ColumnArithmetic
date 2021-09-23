20861ccedb1ae49bc5c448bf2d3aebe1ece847d4
LTI <LTI@INFEDL31055.lntinfotech.com>
2021-09-23 12:00:55 -0500

    added div, sqrt, negate manipulation for one column

--- Program.cs
+++ Program.cs
@@ -11,6 +11,16 @@ namespace ColumnArithmetic
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello World!");
+            Func<float, float, float> sum = (a, b) => a + b;
+            Func<float, float, float> sub = (a, b) => a - b;
+            Func<float, float, float> mult = (a, b) => a * b;
+            Func<float, float, float> div = (a, b) => a / b;
+            Func<float, float> sqrt = (a) => (float)Math.Sqrt(a);
+            Func<float, float> negate = (a) => -a;
+            Func<float, float, float> pow = (a, b) => (float)Math.Pow(a, b);
+            Action<List<Dictionary<string, float>>, Func<float, float, float>, string, string, string> doFunction = (dataList, someFunc, col1, col2, col3) =>  dataList.ForEach(dict => dict[col3] = someFunc(dict[col1], dict[col2]));
+            Action<List<Dictionary<string, float>>, Func<float, float>, string, string> doFunctionSimple = (dataList, someFunc, col1, col2) =>  dataList.ForEach(dict => dict[col2] = someFunc(dict[col1]));
+
 
             var jsonData = File.ReadAllText("./data/generic.json");
 
@@ -43,54 +53,65 @@ namespace ColumnArithmetic
 
                 if (splitString.Length == 4)
                 {
+                    var col1 = splitString[1];
+                    var col2 = splitString[2];
+                    var col3 = splitString[3];
+
                     if (splitString[0] == "add") // > add A B C
                     {
-                        foreach (Dictionary<string, float> dict in dataList)
-                        {
-                            dict[splitString[3]] = dict[splitString[1]] + dict[splitString[2]];
-                        }
-
+                        doFunction(dataList, sum, col1, col2, col3);
                         PrintDictionaryList(dataList);
                     }
                     else if (splitString[0] == "sub") // > sub A B C
                     {
-                        foreach (Dictionary<string, float> dict in dataList)
-                        {
-                            dict[splitString[3]] = dict[splitString[1]] - dict[splitString[2]];
-                        }
-
+                        doFunction(dataList, sub, col1, col2, col3);
                         PrintDictionaryList(dataList);
                     }
                     else if (splitString[0] == "mult") // > mult A B C
                     {
-                        foreach (Dictionary<string, float> dict in dataList)
-                        {
-                            dict[splitString[3]] = dict[splitString[1]] * dict[splitString[2]];
-                        }
-
+                        doFunction(dataList, mult, col1, col2, col3);
                         PrintDictionaryList(dataList);
                     }
                     else if (splitString[0] == "pow") // pow A B C
-                    {
-                        foreach (Dictionary<string, float> dict in dataList)
-                        {
-                            float product = 1;
-
-                            for (int i = 0; i < dict[splitString[2]]; i++)
-                            {
-                                product *= dict[splitString[1]];
-                            }
-
-                            dict[splitString[3]] = product;
-                        }
-
+                    { 
+                        doFunction(dataList, pow, col1, col2, col3);
                         PrintDictionaryList(dataList);
+                    }
+                    else if (splitString[0] == "div") // div A B C
+                    { 
+                        doFunction(dataList, div, col1, col2, col3);
+                        PrintDictionaryList(dataList);
+                    }
+                     else
+                    {
+                         Console.WriteLine("Invalid command.");
+                    }
+                }
+                else if(splitString.Length==3)
+                {
+                    var col1 = splitString[1];
+                    var col2 = splitString[2];
+
+                    if (splitString[0] == "sqrt") // > sqrt A B
+                    {
+                        doFunctionSimple(dataList, sqrt, col1, col2);
+                        PrintDictionaryList(dataList);
+                    }
+                    else if (splitString[0] == "negate") // > negate A B
+                    {
+                        doFunctionSimple(dataList, negate, col1, col2);
+                        PrintDictionaryList(dataList);
+                    }
+                    else
+                    {
+                         Console.WriteLine("Invalid command.");
                     }
                 }
                 else
                 {
                     Console.WriteLine("Invalid command.");
                 }
+
 
             }
         }
